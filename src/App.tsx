2c837fe0088d95421dd17dd3194350bfa5d98d58
yejz0715 <yejz0715@gmail.com>
2024-01-09 22:51:31 +0900

    Feat : 프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>vite로 react 프로젝트 구축하기</h1>;
+}
+
+export default App;
---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+//타입설정
+
+declare module "*.svg" {
+    const src: string;
+    export default src;
+}
